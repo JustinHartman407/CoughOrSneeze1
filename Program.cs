cb81458bd62ab0599df9d63788701be17d4fa35d
JustinHartman407 <50879948+JustinHartman407@users.noreply.github.com>
2019-09-12 17:04:15 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace CoughOrSneeze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Did you cough or sneeze");
+            string action = (Console.ReadLine());
+
+            Console.WriteLine("How many times was it done ");
+            int number = int.Parse(Console.ReadLine());
+
+            try
+            {
+                if (action == "Cough ")
+                {
+                    Console.WriteLine("Bless you ");
+
+                    int i = 1;
+                    do
+                    {
+                        Console.WriteLine("Bless you ");
+                        i++;
+                    }
+
+                    while (i < number);
+                }
+
+                else
+                {
+                    Console.WriteLine("Excuse you");
+
+                    int i = 1;
+                    do
+                    {
+                        Console.WriteLine("Excuse you ");
+                        i++;
+                    }
+
+                    while (i < number);
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+               }
+
+                
+            
+
+        }
+    }
+
